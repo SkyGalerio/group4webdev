3bf97bf33dd0895a7a87e2a56f6f33341619bfb0
sabaflz <sabaa.feilizadeh@gmail.com>
2024-05-21 16:56:55 -0700

    simple React website

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
-import Contact from './components/Contact';
 import Projects from './components/Projects';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
@@ -14,7 +13,6 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
         <Route path="/projects" element={<Projects />} />
       </Routes>
       <Footer />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,6 @@ const Navbar = () => {
       <ul>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
         <li><Link to="/projects">Projects</Link></li>
       </ul>
     </nav>
