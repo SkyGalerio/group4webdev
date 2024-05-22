74ceadee417bae55b4f9d342b2c261c4d4b0005e
sabaflz <sabaa.feilizadeh@gmail.com>
2024-05-21 17:48:38 -0700

    change the color of the buttons

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,13 @@ import '../assets/Home.css'
 
 const Home = () => {
   return (
-    <div>
-      <h1>Welcome to the Developer's Guild</h1>
-    </div>
+
+  <div className="home">
+    <h1>Welcome to the Developer's Guild</h1>
+    <p>Your go-to place for tech enthusiasts at De Anza College.</p>
+    <button>Learn More</button>
+  </div>
+  
   );
 }
 
