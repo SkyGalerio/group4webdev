7464ae78a8d5405fd57b29c8c1121d80d4b08723
sabaflz <sabaa.feilizadeh@gmail.com>
2024-05-21 18:04:42 -0700

    add a simple body to projects page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,8 +3,15 @@ import '../assets/Projects.css'
 
 const Projects = () => {
   return (
-    <div>
-      <h1>Welcome to the Developer's Guild</h1>
+    <div className="projects">
+      <h1>Past Projects</h1>
+      <div className="project-list">
+        <div className="project">
+          <h2>Project Title</h2>
+          <p>Project description goes here.</p>
+        </div>
+        {/* Repeat for more projects */}
+      </div>
     </div>
   );
 }
